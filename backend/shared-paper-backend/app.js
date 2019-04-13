772dc632632a895d6ad609a79d48bc36957536c9
prcao <patrickrcao@gmail.com>
2019-04-13 03:25:06 -0400

    backend stuff

--- backend/shared-paper-backend/app.js
+++ backend/shared-paper-backend/app.js
@@ -1,20 +1,76 @@
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const MongoClient = require('mongodb').MongoClient;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Will 404 if you try to access using HTTP
+app.get('*', (req, res) => {
+    res.sendStatus(404);
+});
+
+// Connect to MongoDB database
+MongoClient.connect(process.env.PORT || 3000, { useNewUrlParser: true })
+    .then(client => {
+
+        const db = client.db();
+        var collection;
+        
+        io.on('connection', socket => {
+            
+            console.log('User connected');
+
+            // Inital connection
+            socket.on('paper/connection', (paperName, callback) => {
+                
+                collection = db.collection(paperName);
+                socket.join(paperName);
+                socket.paper = paperName;
+
+                // get all docunents
+                collection.find().toArray((err, array) => {
+                    
+                    if(err) {
+                        console.log(err);
+                    }
+
+                    callback(array);
+                });
+
+            });
+
+            socket.on('paper/add_path', (path) => {
+                
+                if(collection) {
+                    collection.updateOne(
+                        { 'path.id': path.id },
+                        { path: path },
+                        {
+                            upsert: true
+                        }
+                    )
+                    .then(res => {
+                        return collection.findOne({ 'path.id': path.id })
+                    }).then(res => {
+                        socket.emit('paper/update', res);   // emit the updated path
+                    });
+                }
+
+            });
+
+        });
+
+    })
+    .then(err => {
+        console.log(err);
+    });
+
+
+
+
 
 module.exports = app;
