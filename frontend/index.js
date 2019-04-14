0cf707ccc8c4d8a62e7804243879d33075b45b20
Andy Xiao <andyxiao@seas.upenn.edu>
2019-04-13 23:06:36 -0400

    Add socket.io

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,11 @@
 import { AppRegistry } from 'react-native';
 import Canvas from './src/Canvas';
-import Example from './src/Example';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => Canvas);
+
+console.ignoredYellowBox = ['Remote debugger'];
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+]);
