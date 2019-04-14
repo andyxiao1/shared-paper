7e21d13e0709e2a15a096813011cb258ec58e841
Andy Xiao <andyxiao@seas.upenn.edu>
2019-04-13 20:50:48 -0400

    Implement canvas

---
+++ frontend/index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import Canvas from './src/Canvas';
+import Example from './src/Example';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => Canvas);
